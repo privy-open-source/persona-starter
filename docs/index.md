4235999c617f6b45d5881f726371f74a77e43f87
Ade Novid <adenvt@gmail.com>
2022-09-12 13:41:09 +0700

    feat(docs): add vitepress

---
+++ docs/index.md
@@ -0,0 +1,1 @@
+# Hello Persona + VitePress
---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{md,vue}"],
+  theme  : { extend: {} },
+  plugins: [],
+  presets: [require('@privyid/tailwind-preset')]
+}
